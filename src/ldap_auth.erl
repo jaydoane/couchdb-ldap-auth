3163b98fae2541a0972c264943dbedd3ac8a2c5a
Jay Doane <jay.s.doane@gmail.com>
2015-08-30 01:33:40 -0700

    WIP

--- src/ldap_auth.erl
+++ src/ldap_auth.erl
@@ -1,164 +1,35 @@
 -module(ldap_auth).
--author("jdoane@us.ibm.com").
 
 -include_lib("couch/include/couch_db.hrl").
--include_lib("eldap/include/eldap.hrl").
 
 %% API
 
 -export([default_authentication_handler/1,
+         cookie_authentication_handler/1,
          handle_session_req/1]).
 
--compile([export_all]). % FIXME
+%% -compile([export_all]). % FIXME
 
 -define(SESSION_COOKIE, "LDAPAuthSession").
 -define(TIMESTAMP_ENCODE_BASE, 16).
 
--define(SECTION, "ldap_auth").
-
--define(CLASS_ATTRIBUTE, "objectClass").
-
-config(servers) ->
-    config:get(?SECTION, "servers", ["127.0.0.1"]);
-config(port) ->
-    config:get_integer(?SECTION, "port", 10389);
-config(ssl_port) ->
-    config:get_integer(?SECTION, "ssl_port", 10636);
-config(use_ssl) ->
-    list_to_existing_atom(config:get(?SECTION, "use_ssl", "false"));
-config(connect_timeout) ->
-    config:get_integer(?SECTION, "timeout", 5000);
 config(cookie_timeout) ->
     config:get_integer("couch_httpd_auth", "timeout", 600);
-
-config(user_base_dn) ->
-    config:get(?SECTION, "user_base_dn", "ou=users,dc=example,dc=com");
-config(user_uid_attribute) ->
-    config:get(?SECTION, "user_uid_attribute", "uid");
-config(user_password_attribute) ->
-    config:get(?SECTION, "user_password_attribute", "userPassword");
-
-config(group_base_dn) ->
-    config:get(?SECTION, "group_base_dn", "ou=groups,dc=example,dc=com");
-config(group_search_class) ->
-    config:get(?SECTION, "group_search_class", "posixGroup");
-config(group_member_attribute) ->
-    config:get(?SECTION, "group_member_attribute", "memberUid");
-config(group_role_attribute) ->
-    config:get(?SECTION, "group_role_attribute", "description");
-
-%% FIXME search_user hopefully unnecessary...
-config(search_user_dn) ->
-    config:get(?SECTION, "search_user_dn",
-               "uid=ldapsearch,ou=users,dc=example,dc=com");
-config(search_user_password) ->
-    config:get(?SECTION, "search_user_password", "secret");
-
 config(secret) ->
     case config:get("couch_httpd_auth", "secret", undefined) of
         undefined ->
             undefined;
         Str when is_list(Str) ->
             ?l2b(Str)
-    end;
-
-config(_) ->
-    throw(unknown_config_param).
-
-
-
-user_dn(Uid) ->
-    Prefix = io_lib:format("~s=~s,", [config(user_uid_attribute), Uid]),
-    lists:flatten([Prefix, config(user_base_dn)]).
-
-user_search(Uid) ->
-    Filter = eldap:equalityMatch(config(user_uid_attribute), Uid),
-    #eldap_search{base=config(user_base_dn),
-                  filter=Filter,
-                  attributes=[config(user_password_attribute)]}.
-
-group_search() ->
-    %% FIXME to support multiple group search classes
-    Filter = eldap:equalityMatch(?CLASS_ATTRIBUTE, config(group_search_class)),
-    #eldap_search{base=config(group_base_dn),
-                  filter=Filter,
-                  attributes=[config(group_member_attribute),
-                              config(group_role_attribute)]}.
-
-open() ->
-    BaseOpts = [{timeout, config(connect_timeout)}],
-    Opts = case config(use_ssl) of
-               true ->
-                   [{ssl, true},
-                    {port, config(ssl_port)}];
-               false ->
-                   [{port, config(port)}]
-           end ++ BaseOpts,
-    eldap:open(config(servers), Opts).
-
-
-authorized_roles(Uid, Password) ->
-    {ok, Handle} = open(),
-    try eldap:simple_bind(Handle, user_dn(Uid), Password) of
-        ok ->
-            case eldap:search(Handle, group_search()) of
-                {ok, #eldap_search_result{entries=Entries}} ->
-                    {ok, roles(Uid, Entries)};
-                Else ->
-                    Else
-            end;
-        Else ->
-            Else
-    after
-        eldap:close(Handle)
     end.
 
-roles(Uid, Entries) ->
-    roles(Uid, Entries, []).
-
-roles(_Uid, [], Acc) ->
-    [fixup_role(Role) || Role <- lists:usort(Acc)];
-roles(Uid, [#eldap_entry{attributes=Attributes}|Rest], Acc) ->
-    Uids = proplists:get_value(config(group_member_attribute), Attributes),
-    case lists:member(Uid, Uids) of
-        false ->
-            roles(Uid, Rest, Acc);
-        true ->
-            Roles = proplists:get_value(config(group_role_attribute), Attributes),
-            roles(Uid, Rest, Acc ++ Roles)
-    end.
-
-fixup_role("server_admin") ->
-    server_admin;
-fixup_role(Role) when is_list(Role) ->
-    ?l2b(Role).
-
-search_user(Uid) ->
-    {ok, Handle} = open(),
-    DN = config(search_user_dn),
-    Password = config(search_user_password),
-    try eldap:simple_bind(Handle, DN, Password) of
-        ok ->
-            case eldap:search(Handle, user_search(Uid)) of
-                {ok, #eldap_search_result{
-                        entries=[#eldap_entry{
-                                    attributes=[{_, [EncodedPassword]}]}]}} ->
-                    {ok, EncodedPassword};
-                Error ->
-                    Error
-            end;
-        Error ->
-            Error
-    after
-        eldap:close(Handle)
-    end.
 
 default_authentication_handler(Req) ->
     case couch_httpd_auth:basic_name_pw(Req) of
         {Username, Password} ->
             couch_log:notice("ldap_authentication_handler ~p:~p",
                              [Username, Password]),
-            case authorized_roles(Username, Password) of
+            case ldap_interface:authorized_roles(Username, Password) of
                 {ok, Roles} ->
                     couch_log:notice("ldap_authentication_handler roles ~p",
                                      [Roles]),
@@ -193,7 +64,7 @@ form(MochiReq) ->
 
 handle_session_req(#httpd{method='POST'}=Req) ->
     {Username, Password} = form_credentials(Req),
-    case authorized_roles(Username, Password) of
+    case ldap_interface:authorized_roles(Username, Password) of
         {ok, Roles} ->
             Header = session_header(Req, Username, Roles),
             Json = {[{ok, true}, {name, ?l2b(Username)}, {roles, Roles}]},
@@ -247,7 +118,8 @@ encode_role(Bin) when is_binary(Bin) ->
 
 
 decode_roles(RolesStr) ->
-    [fixup_role(Role) || Role <- re:split(RolesStr, ",", [{return, list}])].
+    [ldap_interface:fixup_role(Role)
+     || Role <- re:split(RolesStr, ",", [{return, list}])].
 
 
 encode_timestamp(Timestamp) ->
---
+++ src/ldap_interface.erl
@@ -0,0 +1,134 @@
+-module(ldap_interface).
+
+-export([authorized_roles/2,
+         search_user/1]).
+
+-include_lib("eldap/include/eldap.hrl").
+
+-define(SECTION, "ldap_auth").
+
+-define(CLASS_ATTRIBUTE, "objectClass").
+
+config(servers) ->
+    config:get(?SECTION, "servers", ["127.0.0.1"]);
+config(port) ->
+    config:get_integer(?SECTION, "port", 10389);
+config(ssl_port) ->
+    config:get_integer(?SECTION, "ssl_port", 10636);
+config(use_ssl) ->
+    list_to_existing_atom(config:get(?SECTION, "use_ssl", "false"));
+config(connect_timeout) ->
+    config:get_integer(?SECTION, "timeout", 5000);
+
+config(user_base_dn) ->
+    config:get(?SECTION, "user_base_dn", "ou=users,dc=example,dc=com");
+config(user_uid_attribute) ->
+    config:get(?SECTION, "user_uid_attribute", "uid");
+config(user_password_attribute) ->
+    config:get(?SECTION, "user_password_attribute", "userPassword");
+
+config(group_base_dn) ->
+    config:get(?SECTION, "group_base_dn", "ou=groups,dc=example,dc=com");
+config(group_search_class) ->
+    config:get(?SECTION, "group_search_class", "posixGroup");
+config(group_member_attribute) ->
+    config:get(?SECTION, "group_member_attribute", "memberUid");
+config(group_role_attribute) ->
+    config:get(?SECTION, "group_role_attribute", "description");
+
+%% FIXME search_user hopefully unnecessary...
+config(search_user_dn) ->
+    config:get(?SECTION, "search_user_dn",
+               "uid=ldapsearch,ou=users,dc=example,dc=com");
+config(search_user_password) ->
+    config:get(?SECTION, "search_user_password", "secret");
+
+config(_) ->
+    throw(unknown_config_param).
+
+
+user_dn(Uid) ->
+    Prefix = io_lib:format("~s=~s,", [config(user_uid_attribute), Uid]),
+    lists:flatten([Prefix, config(user_base_dn)]).
+
+user_search(Uid) ->
+    Filter = eldap:equalityMatch(config(user_uid_attribute), Uid),
+    #eldap_search{base=config(user_base_dn),
+                  filter=Filter,
+                  attributes=[config(user_password_attribute)]}.
+
+group_search() ->
+    %% FIXME to support multiple group search classes
+    Filter = eldap:equalityMatch(?CLASS_ATTRIBUTE, config(group_search_class)),
+    #eldap_search{base=config(group_base_dn),
+                  filter=Filter,
+                  attributes=[config(group_member_attribute),
+                              config(group_role_attribute)]}.
+
+open() ->
+    BaseOpts = [{timeout, config(connect_timeout)}],
+    Opts = case config(use_ssl) of
+               true ->
+                   [{ssl, true},
+                    {port, config(ssl_port)}];
+               false ->
+                   [{port, config(port)}]
+           end ++ BaseOpts,
+    eldap:open(config(servers), Opts).
+
+
+authorized_roles(Uid, Password) ->
+    {ok, Handle} = open(),
+    try eldap:simple_bind(Handle, user_dn(Uid), Password) of
+        ok ->
+            case eldap:search(Handle, group_search()) of
+                {ok, #eldap_search_result{entries=Entries}} ->
+                    {ok, roles(Uid, Entries)};
+                Else ->
+                    Else
+            end;
+        Else ->
+            Else
+    after
+        eldap:close(Handle)
+    end.
+
+roles(Uid, Entries) ->
+    roles(Uid, Entries, []).
+
+roles(_Uid, [], Acc) ->
+    [fixup_role(Role) || Role <- lists:usort(Acc)];
+roles(Uid, [#eldap_entry{attributes=Attributes}|Rest], Acc) ->
+    Uids = proplists:get_value(config(group_member_attribute), Attributes),
+    case lists:member(Uid, Uids) of
+        false ->
+            roles(Uid, Rest, Acc);
+        true ->
+            Roles = proplists:get_value(config(group_role_attribute), Attributes),
+            roles(Uid, Rest, Acc ++ Roles)
+    end.
+
+fixup_role("server_admin") ->
+    server_admin;
+fixup_role(Role) when is_list(Role) ->
+    list_to_binary(Role).
+
+search_user(Uid) ->
+    {ok, Handle} = open(),
+    DN = config(search_user_dn),
+    Password = config(search_user_password),
+    try eldap:simple_bind(Handle, DN, Password) of
+        ok ->
+            case eldap:search(Handle, user_search(Uid)) of
+                {ok, #eldap_search_result{
+                        entries=[#eldap_entry{
+                                    attributes=[{_, [EncodedPassword]}]}]}} ->
+                    {ok, EncodedPassword};
+                Error ->
+                    Error
+            end;
+        Error ->
+            Error
+    after
+        eldap:close(Handle)
+    end.
